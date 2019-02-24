f040d52ee3e4b045a05d1e078cc5003d9e1e6167
gherka <germanpriks@gmail.com>
2019-02-24 12:49:58 +0000

    Added a table with basic descriptive statistics of common variables

--- summary_stats.py
+++ summary_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import defaultdict
 
 def generate_summary():
 
@@ -9,13 +10,32 @@ def generate_summary():
     shape_1 = df1.shape
     shape_2 = df2.shape
 
-    #Find out common variables between the two datasets:
+    #Find out different variable names between the two datasets:
 
     diff_vars = ([(a, 'DF1') for a in df1.columns.values if a not in df2.columns.values] + 
                  [(b, 'DF2') for b in df2.columns.values if b not in df1.columns.values])
 
+    #Find out common variable names between the two datasets
+    common_var_names = [a for a in df1.columns.values if a in df2.columns.values]
 
-    common_vars = [a for a in df1.columns.values if a in df2.columns.values]
+    #Build a nested dictionary of common column metadata
+    common_vars = {}
+
+    d = {'DFs':{'DF1':{'Uniques':0, 'NAs':0}, 'DF2':{'Uniques':0, 'NAs':0}}}
+    dd = defaultdict(dict, d)
+
+    for col in common_var_names:
+
+        common_vars[col] = dict(dd) 
+
+        #collect information from the first dataframe
+        common_vars[col]['DFs']['DF1']['Uniques'] = df1[col].nunique()
+        common_vars[col]['DFs']['DF1']['NAs'] = sum(df1[col].isna())
+        
+        #collect information from the second dataframe
+        common_vars[col]['DFs']['DF2']['Uniques'] = df2[col].nunique()
+        common_vars[col]['DFs']['DF2']['NAs'] = sum(df2[col].isna())
+
 
     if len(diff_vars) == 0 :
         diff_vars = ['None']
