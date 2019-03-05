2aa2c6cc16f900ebc104c9570df7add884a0ef73
gherka <germanpriks@gmail.com>
2019-03-05 11:33:56 +0000

    Added a class for VisRow to make adding multiple new plots easier

--- tkinter_app.py
+++ tkinter_app.py
@@ -2,8 +2,6 @@
 NEED TO THINK ABOUT THE BEST WAY TO PASS DATA BETWEEN
 TKINTER, SEABORN_PLOTS AND SUMMARY_STATS; MAYBE PICKLE?
 Use subprocess call to add support for running external scripts
-
-Re-factor into more meaningful classes
 '''
 
 from tkinter import Tk, Label, Button, filedialog, StringVar, OptionMenu, Frame, W, LEFT
@@ -14,56 +12,72 @@ import pandas as pd
 from jinja_app import generate_report
 from summary_stats import generate_summary
 
+class VisRow:
+    def __init__(self, master):
+        #save the master reference for internal use of this class outside of __init__
+        self.mainMaster = master
+
+        self.visContainer = Frame(master.mainContainer)
+
+        self.dropdownText = "Choose a common variable to plot"
+        self.dropdownLabel=Label(self.visContainer, text=self.dropdownText, wraplength=110, anchor=W, justify=LEFT)
+        self.dropdownLabel.grid(row=0, column=0, pady=(5,20), sticky=W)
+
+        self.choices = ['None']
+
+        self.popupMenu = ttk.Combobox(self.visContainer, values=self.choices, postcommand=self.populateDropdown)
+        self.popupMenu.grid(row=0, column=1, padx=10, pady=(5,20), sticky=W)
+        self.popupMenu.current(0)
+
+        #on change dropdown value
+        self.popupMenu.bind("<<ComboboxSelected>>", self.callbackFunc)
+
+    def populateDropdown(self):
+        #import the common values from BasicGui
+        self.popupMenu['values']=self.mainMaster.common_values
+
+    def callbackFunc(self, event):
+        #export the callback value to the master
+        self.mainMaster.var_to_plot = self.popupMenu.get()
+
+
 class BasicGUI:
     #CHILD OF ROOT (TOP-LEVEL WINDOW)
     def __init__(self, master):
 
-        self.myContainer = Frame(master)
-        self.myContainer.grid(row=0, column=1)
+        self.mainContainer = Frame(master)
+        self.mainContainer.grid(row=0, column=1)
 
         self.master = master
 
         master.title("Dataset comparison tool")
 
-        self.label = (Label(self.myContainer, text="Use the buttons to operate the tool!")
+        self.label = (Label(self.mainContainer, text="Use the buttons to operate the tool!")
                             .grid(row=0, column=0, columnspan=2, pady=10, padx=5))
 
-        self.first_button = Button(self.myContainer, text="Pick the First File", command=self.open_file_1)
+        self.first_button = Button(self.mainContainer, text="Pick the First File", command=self.open_file_1)
         self.first_button.grid(row=1, column=0, sticky=W)
 
-        self.second_button = Button(self.myContainer, text="Pick the Second File", command=self.open_file_2)
+        self.second_button = Button(self.mainContainer, text="Pick the Second File", command=self.open_file_2)
         self.second_button.grid(row=2, column=0, sticky=W)
 
         #Set defaults for testing
         self.filename_1 = 'C:\\Users\\germap01\\Python\\UNSORTED\\Hackathon\\2019\\Working\\data_1.csv'
         self.filename_2 = 'C:\\Users\\germap01\\Python\\UNSORTED\\Hackathon\\2019\\Working\\data_2.csv'
         
-        #Create a dropdown
+        #Common values for populating dropdowns:
+        self.common_values = list(generate_summary(self.filename_1, self.filename_2)['Metadata']['common_vars'].keys())
+        
+        #Create the first vis row:
+        self.visrow_1 = VisRow(self)
+        self.visrow_1.visContainer.grid(row=3, column=0, columnspan=2)
 
-        def populateDropdown():
-
-            common_values = list(generate_summary(self.filename_1, self.filename_2)['Metadata']['common_vars'].keys())
-            popupMenu['values']=common_values
-
-        dropdownText = "Choose a common variable to plot"
-        dropdownLabel=Label(self.myContainer, text=dropdownText, wraplength=110, anchor=W, justify=LEFT)
-        dropdownLabel.grid(row=3, column=0, pady=(5,20), sticky=W)
-
-        self.choices = ['None']
-        popupMenu = ttk.Combobox(self.myContainer, values=self.choices, postcommand=populateDropdown)
-        popupMenu.grid(row=3, column=1, padx=10, pady=(5,20), sticky=W)
-        popupMenu.current(0)
-
-        # on change dropdown value
-        def callbackFunc(event):
-            self.var_to_plot = popupMenu.get()
-    
-        popupMenu.bind("<<ComboboxSelected>>", callbackFunc)
-
-        self.jinja_button = Button(self.myContainer, text="MAGIC!", command=self.run_jinja)
+        #Magic Button
+        self.jinja_button = Button(self.mainContainer, text="MAGIC!", command=self.run_jinja)
         self.jinja_button.grid(row=4, column=0, sticky=W)
 
-        self.close_button = Button(self.myContainer, text="CLOSE APP", command=self.myContainer.quit)
+        #Close App Button
+        self.close_button = Button(self.mainContainer, text="CLOSE APP", command=self.mainContainer.quit)
         self.close_button.grid(row=5, column=0, sticky=W)
 
     #FUNCTIONS:
@@ -80,7 +94,7 @@ root = Tk()
 root.geometry("320x400")
 my_gui = BasicGUI(root)
 
-#Position the myContainer in the middle of the Main Window 3x3 grid
+#Position the mainContainer in the middle of the Main Window 3x3 grid
 root.grid_rowconfigure(1, weight=1)
 root.grid_rowconfigure(2, weight=1)
 root.grid_columnconfigure(0, weight=1)
