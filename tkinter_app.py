aacfdeab3d91b0bb3b39fec788ef9d44599739f4
gherka <germanpriks@gmail.com>
2019-03-26 11:58:17 +0000

    Added highlight on selected radio button

--- tkinter_app.py
+++ tkinter_app.py
@@ -12,6 +12,16 @@ from core.jinja_app import generate_report
 from core.summary_stats import generate_summary
 from core.helper_funcs import read_data, dtype_mapping
 
+class GroupedRadioButton(Radiobutton):
+    """
+    Extend RadioButton class to add a setter and getter methods for group name
+    """
+    def set_group_name(self, name):
+        self.group = name
+
+    def get_group_name(self):
+        return self.group
+
 class DataTypePopup:
     def __init__(self, gui, master):
         #save the master reference for internal use
@@ -34,7 +44,7 @@ class DataTypePopup:
         self.toplevel.grid_columnconfigure(2, weight=1)
 
         self.popup_headline = "Please confirm data types for each common variable:"
-        Label(self.popup, text=self.popup_headline).grid(row=0, columnspan=5, sticky=W+E)
+        Label(self.popup, text=self.popup_headline, font=("Helvetica", 12), pady=8, padx=10).grid(row=0, columnspan=5, sticky=W+E)
 
         self.dtype_radio_dict = {}
         self.dtype_choices = ['Categorical', 'Continuous', 'Timeseries']
@@ -42,37 +52,61 @@ class DataTypePopup:
         #Create the header row
         for i, val in enumerate(self.dtype_choices):
             #keep the first column empty for row headers
-            Label(self.popup, text=f"{val}").grid(row=1, column=i+1)
+            Label(self.popup, text=f"{val}", pady=10, font=("Helvetica", 10)).grid(row=1, column=i+1)
 
-        #Create radio button "rows"
+        #Create radio button "rows" and put grouped radio buttons into a dictionary for group lookup
+        self.radio_groups = {}
+
         for i, var in enumerate(gui.common_values):
+
+            self.radio_groups[var] = []
 
             self.dtype_radio_dict[var] = StringVar()
             self.dtype_radio_dict[var].set(dtype_mapping(gui.dtypes[var]))
 
             #first row is headline, second row is headers
-            Label(self.popup, text=f"{var}").grid(row=i+2, column=0, sticky=W)
+            Label(self.popup, text=f"{var}", font=("Helvetica", 10), padx=3).grid(row=i+2, column=0, sticky=W)
 
             for j, val in enumerate(self.dtype_choices):
             
-                btn = Radiobutton(
+                btn = GroupedRadioButton(
                                 self.popup,
                                 text="",
                                 variable=self.dtype_radio_dict[var],
-                                value=val
+                                value=val,
+                                background='bisque' if val == self.dtype_radio_dict[var].get() else self.toplevel['bg'],
+                                padx=5,
+                                pady=5
                                 )
+                #set the group name and append to the dict
+                btn.set_group_name(var)
+                self.radio_groups[var].append(btn)
 
+                #bind the button click to a function
+                btn.bind("<Button-1>", lambda e, w=btn: self.RadioButtonClick(w))
+
+                #position the radio button
                 btn.grid(row=i+2, column=j+1)
 
         #Create Confirm and Exit button
-        Button(self.popup, text="Done", command=self.ConfimDataTypes).grid(row=i+3, column=4)
+        Button(self.popup, text="Done", command=self.ConfimDataTypes, padx=5, pady=5).grid(row=i+3, column=4)
 
     #CLASS FUNCTIONS:
     def ConfimDataTypes(self):
         print([(key, value.get()) for key, value in self.dtype_radio_dict.items()])
         self.toplevel.destroy()
 
-        
+    def RadioButtonClick(self, widget):
+        """
+        Change background colour of the selected radio button
+        """
+        for w in self.radio_groups[widget.get_group_name()]:
+            
+            if w is widget:
+                w.configure(background='bisque')
+            else:
+                w.configure(background=self.toplevel['bg'])
+
 class VisRow:
     def __init__(self, master):
         #save the master reference (my_gui) for internal use
