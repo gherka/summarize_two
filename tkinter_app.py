d96c507d4a7fa364923f028a493ff302b56c3144
gherka <germanpriks@gmail.com>
2019-04-27 23:49:27 +0100

    Refactored tkinter_app.py for clarity and better user experience

--- tkinter_app.py
+++ tkinter_app.py
@@ -1,6 +1,5 @@
 from tkinter import Tk, Label, Button, filedialog, StringVar, OptionMenu, Frame, Toplevel, Message, Radiobutton
-from tkinter import W, E, LEFT
-from tkinter import ttk
+from tkinter import W, E, CENTER, LEFT
 import os
 import pandas as pd
 
@@ -20,6 +19,10 @@ class GroupedRadioButton(Radiobutton):
         return self.group
 
 class DataTypePopup:
+    '''
+    Pop up window to allow users to amend default data types
+    '''
+
     def __init__(self, gui, master):
         #save the master reference for internal use
         self.mainMaster = master
@@ -89,7 +92,10 @@ class DataTypePopup:
         #Create Confirm and Exit button
         Button(self.popup, text="Done", command=self.ConfimDataTypes, padx=5, pady=5).grid(row=i+3, column=4)
 
-    #CLASS FUNCTIONS:
+    ###########################
+    # DATA TYPE CLASS METHODS #
+    ###########################
+
     def RadioButtonClick(self, widget):
         """
         Change background colour of the selected radio button
@@ -103,13 +109,22 @@ class DataTypePopup:
 
     def ConfimDataTypes(self):
         """
-        Overwrite dtypes dictionary from main GUI and exit popup
+        Overwrite dtypes dictionary from main GUI, enable report buttons and exit popup
         """
         self.mainGui.dtypes = {key:value.get() for key, value in self.dtype_radio_dict.items()}
+        self.mainGui.dt_confirm.config(text="Done!", font="Helvetica 9 italic")
+        self.mainGui.jinja_button.config(state='normal')
+        self.mainGui.ridge_button.config(state='normal')
         self.toplevel.destroy()
 
 class BasicGUI:
+    '''
+    Main user interface of the tool
+    '''
+
     def __init__(self, master):
+
+        self.button_font = "Helvetica 9"
 
         self.master = master
         self.ridge = False #Temporary workaround
@@ -118,52 +133,100 @@ class BasicGUI:
         self.mainContainer = Frame(master)
         self.mainContainer.grid(row=0, column=1)
 
-        self.label = Label(self.mainContainer, text="Use the buttons to operate the tool!")
-        self.label.grid(row=0, column=0, columnspan=2, pady=10, padx=5)
+        #<<<< FIRST ROW OF GUI >>>>#
+        #Welcome text
 
-        self.first_button = Button(self.mainContainer, text="Pick the First File", command=self.open_file_1)
-        self.first_button.grid(row=1, column=0, sticky=W)
+        welcome_text = """This tool generates an HTML report with various metrics to compare two datasets.
+        \nFollow the steps below:"""
 
-        self.second_button = Button(self.mainContainer, text="Pick the Second File", command=self.open_file_2)
-        self.second_button.grid(row=2, column=0, sticky=W)
+        self.label = Label(self.mainContainer, text=welcome_text,
+                          font="Helvetica 12 italic",
+                          wraplength=300,
+                          justify=LEFT,
+                          anchor=W)
+        self.label.grid(row=0, column=0, columnspan=3, pady=20, padx=5, sticky=W)
 
-        #Set defaults for testing; don't forget to press Ready Datasets to read them in
-        self.filename_1 = 'C:\\Users\\germap01\\Python\\UNSORTED\\Hackathon\\2019\\Working\\data_1.csv'
-        self.filename_2 = 'C:\\Users\\germap01\\Python\\UNSORTED\\Hackathon\\2019\\Working\\data_2.csv'
+        #<<<< SECOND ROW OF GUI >>>>#
+        #Ask user to select datasets for comparison
 
-        #Read in the datasets and generate common values
-        self.ready_button = Button(self.mainContainer, text="Read in datasets", command=self.ready_datasets)
-        self.ready_button.grid(row=3, column=0, sticky=W)
+        self.step_1 = Label(self.mainContainer, text="Step 1",
+                            font="Helvetica 12 bold", fg="green")
+        self.step_1.grid(row=1, column=0, rowspan=2, padx=(5, 15), pady=10, sticky=W)
 
+        self.first_button = Button(self.mainContainer, text="Pick the First File", font=self.button_font, command=self.open_file_1)
+        self.first_button.grid(row=1, column=1, sticky=W)
+
+        self.file_label_1 = Label(self.mainContainer, text="")
+        self.file_label_1.grid(row=2, column=1, columnspan=1, pady=5, padx=5)
+
+        self.second_button = Button(self.mainContainer, text="Pick the Second File", font=self.button_font, command=self.open_file_2)
+        self.second_button.grid(row=1, column=2, sticky=W)
+
+        self.file_label_2 = Label(self.mainContainer, text="")
+        self.file_label_2.grid(row=2, column=2, columnspan=1, pady=5, padx=5)
+
+        #<<<< THIRD ROW OF GUI >>>>#
         #Ask the user to confirm datatypes
-        self.dt_button = Button(self.mainContainer, text="Confirm datatypes", command=self.confirm_dt)
-        self.dt_button.grid(row=4, column=0, sticky=W)
+
+        self.step_2 = Label(self.mainContainer, text="Step 2",
+                            font="Helvetica 12 bold", fg="green")
+        self.step_2.grid(row=3, column=0, rowspan=2, padx=(5, 15), pady=10, sticky=W)
+       
+        self.dt_button = Button(self.mainContainer, text="Confirm datatypes", font=self.button_font, state="disabled", command=self.confirm_dt)
+        self.dt_button.grid(row=3, column=1, sticky=W)
+
+        self.dt_confirm = Label(self.mainContainer, text="")
+        self.dt_confirm.grid(row=4, column=1, columnspan=1, pady=5, padx=5)
+
+        #<<<< FOURTH ROW OF GUI >>>>#
+        #Optional elements of the report, like ridge plot or data table
+
+        self.step_3 = Label(self.mainContainer, text="Step 3",
+                            font="Helvetica 12 bold", fg="green")
+        self.step_3.grid(row=5, column=0, rowspan=2, padx=(5, 15), pady=10, sticky=W)
+
+        self.advanced_options = Label(self.mainContainer, text="Optional Report Features", font="Helvetica 11 italic")
+        self.advanced_options.grid(row=5, column=1, columnspan=2, pady=5, padx=5)
         
-        #Ridge Plot with Multiprocessing
-        self.test_button = Button(self.mainContainer, text="Ridge Plot", command=self.ridge_plot)
-        self.test_button.grid(row=5, column=0, sticky=W)
+        self.ridge_button = Button(self.mainContainer, text="Ridge Plot", font=self.button_font, state="disabled", command=self.ridge_plot)
+        self.ridge_button.grid(row=6, column=1, sticky=W)
+
+        #<<<< LAST ROW OF GUI >>>>#
 
         #Magic Button
-        self.jinja_button = Button(self.mainContainer, text="MAGIC!", command=self.run_jinja)
-        self.jinja_button.grid(row=6, column=0, sticky=W)
-
-        #Close App Button
-        self.close_button = Button(self.mainContainer, text="CLOSE APP", command=self.mainContainer.quit)
-        self.close_button.grid(row=7, column=0, sticky=W)
+        self.jinja_button = Button(self.mainContainer, text="GENERATE REPORT",
+                                   font=self.button_font, state="disabled",
+                                   height=3, command=self.run_jinja)
+        self.jinja_button.grid(row=7, column=0, columnspan=3, pady=(20,0))
 
 
-    #CLASS FUNCTIONS:
+    ###########################
+    # BASIC GUI CLASS METHODS #
+    ###########################
+
     def open_file_1(self):
         self.filename_1 = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select file")
+        self.file_label_1.config(text = "Done!", font="Helvetica 9 italic")
     
     def open_file_2(self):
-        self.filename_2 = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select file")
+        '''
+        This function has two additional tasks apart from
+        specifying the file path(1) to the second dataset.
 
-    def ready_datasets(self):
-        #Datasets are read in once for the life-time of the GUI; passed by reference to other modules
+        - It reads in the files as Pandas dataframes(2).
+        - It "readies" the "Confirm Datatypes" button (3)
+
+        Note, dataframes are created once for the life-time of the GUI and passed by reference to other modules
+        '''
+        #TASK 1
+        self.filename_2 = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select file")
+        #TASK 2
         self.df1, self.df2 = read_data(self.filename_1, self.filename_2)
         self.common_values = generate_common_vars(self.df1, self.df2)
         self.dtypes = self.df1[self.common_values].dtypes.to_dict()
+        #TASK 3
+        self.file_label_2.config(text= "Done!", font="Helvetica 9 italic")
+        self.dt_button.config(state='normal')
 
     def confirm_dt(self):
         #self is the my_gui object (instacne of the BasicGUI class), self.master is root widget 
@@ -192,6 +255,8 @@ class BasicGUI:
         else:
             generate_report(self.df1, self.df2, self.dtypes)
 
+        self.jinja_button.config(bg="pale green3")
+        os.startfile(os.path.join(os.getcwd(), 'hello.html'))
 
 if __name__ == "__main__":
 
